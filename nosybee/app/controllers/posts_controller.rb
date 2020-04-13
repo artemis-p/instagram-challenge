083abf773ab909efde762e1d234bee0c393595c2
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-04-13 15:59:45 +0100

    "Added show and list of posts"

--- nosybee/app/controllers/posts_controller.rb
+++ nosybee/app/controllers/posts_controller.rb
@@ -1,9 +1,20 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    render plain: params[:post].inspect
+    @post = Post.new(params.require(:post).permit(:title, :text))
+ 
+    @post.save
+    redirect_to @post
   end
 
 end
