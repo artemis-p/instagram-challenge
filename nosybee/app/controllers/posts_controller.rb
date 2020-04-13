d969d7ad90afbe0f0f1a46643f34cb796d1dfa1c
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-04-13 20:56:47 +0100

    "Added updating posts and bootstrap"

--- nosybee/app/controllers/posts_controller.rb
+++ nosybee/app/controllers/posts_controller.rb
@@ -8,13 +8,37 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
+  end
+
+  def edit
+    @post = Post.find(params[:id])
   end
 
   def create
-    @post = Post.new(params.require(:post).permit(:title, :text))
+    @post = Post.new(post_params)
  
-    @post.save
-    redirect_to @post
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
+  def update
+    @post = Post.find(params[:id])
+   
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+  
+
+end
+
+private
+def post_params
+  params.require(:post).permit(:title, :text)
 end
